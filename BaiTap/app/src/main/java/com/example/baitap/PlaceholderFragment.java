76d4d7eb8be157cdd1b94dd139155b1ccbe69e57
haivt2301 <vanthihai2301@gmail.com>
2026-04-08 14:13:08 +0700

    Initial commit

---
+++ BaiTap/app/src/main/java/com/example/baitap/PlaceholderFragment.java
@@ -0,0 +1,28 @@
+package com.example.baitap;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PlaceholderFragment extends Fragment {
+    private String content;
+
+    public PlaceholderFragment(String content) {
+        this.content = content;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(getContext());
+        textView.setText(content);
+        textView.setGravity(android.view.Gravity.CENTER);
+        textView.setTextSize(24);
+        return textView;
+    }
+}
